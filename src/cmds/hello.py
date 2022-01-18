500df6d54622ef81ca009af702ebfdd531e6dad6
Brian Jarvis <brian.jarvis4@gmail.com>
2022-01-18 10:40:50 -0500

    get random quote from the office api and display instead of saying hello

--- src/cmds/hello.py
+++ src/cmds/hello.py
@@ -1,7 +1,17 @@
 import cowsay
+import requests
+import json
 
 def hello(components):
   '''Say hi back
   '''
 
-  return cowsay.get_output_string('squirrel', 'Hello {0}!'.format(components['sender']))
+  quote_str = """
+  {0}
+    -- {1} {2}"""
+
+  req = requests.get('https://officeapi.dev/api/quotes/random')
+  qt = json.loads(req.text).get('data')
+  return cowsay.get_output_string('squirrel', quote_str.format(qt['content'], qt['character']['firstname'], qt['character']['lastname']))
+  ## old just return hello to the person.  Leaving this here incase we want to respond this way again.
+  # return cowsay.get_output_string('squirrel', 'Hello {0}!'.format(components['sender']))
